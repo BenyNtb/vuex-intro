79c270946124dfc62444eed62823f07166bdc3db
Beny <b.ntambwe97@gmail.com>
2021-12-15 10:33:41 +0100

    intro

---
+++ src/components/Component1.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+      {{count}}
+      {{name}}
+      <button @click="increment">+</button>
+      <div>
+        <input type="text">
+        <button>Add names</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Component1',
+props: {
+msg: String
+},
+computed: {
+    count() {
+        return this.$store.state.count
+    }
+},
+methods: {
+    increment() {
+      this.$store.commit("increment");
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Component1 msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Component1 from '@/components/Component1.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Component1,
+  }
+}
+</script>
